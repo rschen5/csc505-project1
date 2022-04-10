5a6b33ef03489119c998ba5c182751922ccb3f6f
Manali Shirsekar <mnshirse@ncsu.edu>
2022-04-10 17:07:31 -0400

    Add files via upload

---
+++ src/TimSort.java
@@ -0,0 +1,248 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.text.*;
+import java.time.*;
+
+public class TimSort {
+  
+  public static final int INFILE = 0;
+  public static final int OUTFILE = 1;
+  public static int lineCount = 0;
+  public static String[] lines;
+  public static int MIN_MERGE = 32;
+  
+  public static void main(String[] args) {
+    if (args.length != 2) {
+        System.out.println("Usage: java -cp bin Insertion infile outfile");
+        System.exit(1);
+    }
+    
+    Scanner in = null;
+    PrintWriter out = null;
+    
+    try {
+        in = new Scanner(new FileInputStream(args[INFILE]));
+        while(in.hasNextLine()) {
+          in.nextLine();
+          lineCount++;
+        }
+        in = new Scanner(new FileInputStream(args[INFILE]));
+    } 
+    catch (FileNotFoundException e) {
+        System.out.println("Unable to access input file: " + args[INFILE]);
+        System.exit(1);
+    }
+
+    Scanner scnr = new Scanner(System.in);
+    Path path = Path.of(args[OUTFILE]);
+ 
+    try {
+        out = new PrintWriter(new FileOutputStream(args[OUTFILE]));
+    } 
+    catch (FileNotFoundException e) {
+        System.out.println("Cannot create output file");
+        System.exit(1);
+    }
+    
+    try {
+      long startTime = System.nanoTime();
+      readInput(in);
+      long endTime = System.nanoTime();
+      long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+      long durationms = duration;
+      System.out.println("Time to read input: " + durationms + " nanoseconds.");
+      in.close();
+      
+      startTime = System.nanoTime();
+      timsort(lines, lineCount);
+      endTime = System.nanoTime();
+      duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
+      durationms = duration;
+      System.out.println("Time to sort input: " + durationms + " nanoseconds.");
+      
+      writeOutput(out);
+      out.close();
+    } catch (Exception pe) {
+      System.out.println("Caught ParseException during sorting");
+      System.exit(1);
+    }
+  }
+  
+  public static void readInput(Scanner in) {
+    lines = new String[lineCount];
+    for(int i = 0; i < lineCount; i++) {
+      lines[i] = in.nextLine();
+    }
+  }
+  
+  public static LocalDateTime getDate(String dateString) {
+    int year = Integer.parseInt(dateString.substring(0, 4));
+    int month = Integer.parseInt(dateString.substring(5, 7));
+    int date = Integer.parseInt(dateString.substring(8, 10));
+    int hour = Integer.parseInt(dateString.substring(11, 13));
+    int minute = Integer.parseInt(dateString.substring(14, 16));
+    int second = Integer.parseInt(dateString.substring(17, 19));
+    LocalDateTime timestamp = LocalDateTime.of(year, month, date, hour, minute, second, 0);
+    return timestamp;
+  }
+  
+  // public static void sort(String[] dataStrings) throws ParseException {
+  //   //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+  //   for(int i = 1; i < lineCount; i++) {
+  //     Scanner line =  new Scanner(lines[i]);
+  //     String dateStr = line.next();
+  //     LocalDateTime key = getDate(dateStr);
+  //     line.close();
+
+  //     int j = i - 1;
+      
+  //     Scanner jDateScanner = new Scanner(lines[j]);
+  //     String jDateStr = jDateScanner.next();
+  //     LocalDateTime jDate = getDate(jDateStr);
+  //     jDateScanner.close();
+      
+  //     String keyHolder = lines[i];
+  //   }
+  // }
+
+  public static void writeOutput(PrintWriter out) {
+    for(int i = 0; i < lineCount; i++) {
+      out.println(lines[i]);
+    }
+  }
+
+  public static int minRunLength(int n) {
+    assert n >= 0;
+
+    // Becomes 1 if any 1 bits are shifted off
+    int r = 0;
+    while (n >= MIN_MERGE) {
+        r |= (n & 1);
+        n >>= 1;
+    }
+    return n + r;
+  }
+
+  // This function sorts array from left index to
+  // to right index which is of size atmost RUN
+  public static void insertionSort(String[] arr, int left, int right) {
+    for (int i = left + 1; i <= right; i++) {
+        String temp = arr[i];
+        int j = i - 1;
+        while (j >= left && compare(arr[j], temp) > 0) { /**arr[j] > temp*/  // CHANGE
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+  }
+
+  // Merge function merges the sorted runs
+  public static void merge(String[] arr, int l, int m, int r) {
+    // Original array is broken in two parts
+    // left and right array
+    int len1 = m - l + 1, len2 = r - m;
+    String[] left = new String[len1];
+    String[] right = new String[len2];
+    for (int x = 0; x < len1; x++) {
+        left[x] = arr[l + x];
+    }
+    for (int x = 0; x < len2; x++) {
+        right[x] = arr[m + 1 + x];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    // After comparing, we merge those two array
+    // in larger sub array
+    while (i < len1 && j < len2) {
+        if (compare(left[i], right[j]) <= 0)/**left[i] <= right[j]**/ {
+            arr[k] = left[i];
+            i++;
+        }
+        else {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy remaining elements
+    // of left, if any
+    while (i < len1) {
+        arr[k] = left[i];
+        k++;
+        i++;
+    }
+
+    // Copy remaining element
+    // of right, if any
+    while (j < len2) {
+        arr[k] = right[j];
+        k++;
+        j++;
+    }
+  }
+
+  // Iterative Timsort function to sort the
+  // array[0...n-1] (similar to merge sort)
+  public static void timsort(String[] arr, int n) {
+    int minRun = minRunLength(MIN_MERGE);
+    
+    // Sort individual subarrays of size RUN
+    for (int i = 0; i < n; i += minRun) {
+        insertionSort(arr, i, Math.min((i + MIN_MERGE - 1), (n - 1)));
+    }
+
+    // Start merging from size
+    // RUN (or 32). It will
+    // merge to form size 64,
+    // then 128, 256 and so on
+    // ....
+    for (int size = minRun; size < n; size = 2 * size)
+    {
+
+        // Pick starting point
+        // of left sub array. We
+        // are going to merge
+        // arr[left..left+size-1]
+        // and arr[left+size, left+2*size-1]
+        // After every merge, we
+        // increase left by 2*size
+        for (int left = 0; left < n;
+                              left += 2 * size)
+        {
+
+            // Find ending point of left sub array
+            // mid+1 is starting point of right sub
+            // array
+            int mid = left + size - 1;
+            int right = Math.min((left + 2 * size - 1),
+                                  (n - 1));
+
+            // Merge sub array arr[left.....mid] &
+            // arr[mid+1....right]
+              if(mid < right)
+                merge(arr, left, mid, right);
+        }
+    }
+  }
+
+  private static int compare(String first, String second) {
+    Scanner firstLine = new Scanner(first);
+    String firstDateStr = firstLine.next();
+    LocalDateTime firstKey = getDate(firstDateStr);
+    firstLine.close();
+
+    Scanner secondLine = new Scanner(second);
+    String secondDateStr = secondLine.next();
+    LocalDateTime secondKey = getDate(secondDateStr);
+    secondLine.close();
+
+    return(firstKey.compareTo(secondKey));
+  }
+
+}
---
+++ src/timsort.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+echo "A"
+java -cp bin TimSort Project1/A/1.log outputs/A/timsort_1.log
+java -cp bin TimSort Project1/A/2.log outputs/A/timsort_2.log
+java -cp bin TimSort Project1/A/4.log outputs/A/timsort_4.log
+java -cp bin TimSort Project1/A/8.log outputs/A/timsort_8.log
+java -cp bin TimSort Project1/A/16.log outputs/A/timsort_16.log
+java -cp bin TimSort Project1/A/32.log outputs/A/timsort_32.log
+java -cp bin TimSort Project1/A/64.log outputs/A/timsort_64.log
+java -cp bin TimSort Project1/A/128.log outputs/A/timsort_128.log
+java -cp bin TimSort Project1/A/256.log outputs/A/timsort_256.log
+java -cp bin TimSort Project1/A/512.log outputs/A/timsort_512.log
+java -cp bin TimSort Project1/A/1024.log outputs/A/timsort_1024.log
+java -cp bin TimSort Project1/A/2048.log outputs/A/timsort_2048.log
+java -cp bin TimSort Project1/A/4096.log outputs/A/timsort_4096.log
+java -cp bin TimSort Project1/A/8192.log outputs/A/timsort_8192.log
+java -cp bin TimSort Project1/A/16384.log outputs/A/timsort_16384.log
+java -cp bin TimSort Project1/A/32768.log outputs/A/timsort_32768.log
+java -cp bin TimSort Project1/A/65536.log outputs/A/timsort_65536.log
+java -cp bin TimSort Project1/A/131072.log outputs/A/timsort_131072.log
+java -cp bin TimSort Project1/A/262144.log outputs/A/timsort_262144.log
+java -cp bin TimSort Project1/A/524288.log outputs/A/timsort_524288.log
+java -cp bin TimSort Project1/A/1048576.log outputs/A/timsort_1048576.log
+java -cp bin TimSort Project1/A/2097152.log outputs/A/timsort_2097152.log
+java -cp bin TimSort Project1/A/4194304.log outputs/A/timsort_4194304.log
+
+echo ""
+echo "B"
+java -cp bin TimSort Project1/B/1.log outputs/B/timsort_1.log
+java -cp bin TimSort Project1/B/2.log outputs/B/timsort_2.log
+java -cp bin TimSort Project1/B/4.log outputs/B/timsort_4.log
+java -cp bin TimSort Project1/B/8.log outputs/B/timsort_8.log
+java -cp bin TimSort Project1/B/16.log outputs/B/timsort_16.log
+java -cp bin TimSort Project1/B/32.log outputs/B/timsort_32.log
+java -cp bin TimSort Project1/B/64.log outputs/B/timsort_64.log
+java -cp bin TimSort Project1/B/128.log outputs/B/timsort_128.log
+java -cp bin TimSort Project1/B/256.log outputs/B/timsort_256.log
+java -cp bin TimSort Project1/B/512.log outputs/B/timsort_512.log
+java -cp bin TimSort Project1/B/1024.log outputs/B/timsort_1024.log
+java -cp bin TimSort Project1/B/2048.log outputs/B/timsort_2048.log
+java -cp bin TimSort Project1/B/4096.log outputs/B/timsort_4096.log
+java -cp bin TimSort Project1/B/8192.log outputs/B/timsort_8192.log
+java -cp bin TimSort Project1/B/16384.log outputs/B/timsort_16384.log
+java -cp bin TimSort Project1/B/32768.log outputs/B/timsort_32768.log
+java -cp bin TimSort Project1/B/65536.log outputs/B/timsort_65536.log
+java -cp bin TimSort Project1/B/131072.log outputs/B/timsort_131072.log
+java -cp bin TimSort Project1/B/262144.log outputs/B/timsort_262144.log
+java -cp bin TimSort Project1/B/524288.log outputs/B/timsort_524288.log
+java -cp bin TimSort Project1/B/1048576.log outputs/B/timsort_1048576.log
+java -cp bin TimSort Project1/B/2097152.log outputs/B/timsort_2097152.log
+java -cp bin TimSort Project1/B/4194304.log outputs/B/timsort_4194304.log
+
+echo ""
+echo "C"
+java -cp bin TimSort Project1/C/1.log outputs/C/timsort_1.log
+java -cp bin TimSort Project1/C/2.log outputs/C/timsort_2.log
+java -cp bin TimSort Project1/C/4.log outputs/C/timsort_4.log
+java -cp bin TimSort Project1/C/8.log outputs/C/timsort_8.log
+java -cp bin TimSort Project1/C/16.log outputs/C/timsort_16.log
+java -cp bin TimSort Project1/C/32.log outputs/C/timsort_32.log
+java -cp bin TimSort Project1/C/64.log outputs/C/timsort_64.log
+java -cp bin TimSort Project1/C/128.log outputs/C/timsort_128.log
+java -cp bin TimSort Project1/C/256.log outputs/C/timsort_256.log
+java -cp bin TimSort Project1/C/512.log outputs/C/timsort_512.log
+java -cp bin TimSort Project1/C/1024.log outputs/C/timsort_1024.log
+java -cp bin TimSort Project1/C/2048.log outputs/C/timsort_2048.log
+java -cp bin TimSort Project1/C/4096.log outputs/C/timsort_4096.log
+java -cp bin TimSort Project1/C/8192.log outputs/C/timsort_8192.log
+java -cp bin TimSort Project1/C/16384.log outputs/C/timsort_16384.log
+java -cp bin TimSort Project1/C/32768.log outputs/C/timsort_32768.log
+java -cp bin TimSort Project1/C/65536.log outputs/C/timsort_65536.log
+java -cp bin TimSort Project1/C/131072.log outputs/C/timsort_131072.log
+java -cp bin TimSort Project1/C/262144.log outputs/C/timsort_262144.log
+java -cp bin TimSort Project1/C/524288.log outputs/C/timsort_524288.log
+java -cp bin TimSort Project1/C/1048576.log outputs/C/timsort_1048576.log
+java -cp bin TimSort Project1/C/2097152.log outputs/C/timsort_2097152.log
+java -cp bin TimSort Project1/C/4194304.log outputs/C/timsort_4194304.log
+
+echo ""
+
+done
